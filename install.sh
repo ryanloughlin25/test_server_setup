078fefb01fb040e50b84324ba55d0a7059d77e38
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-03-21 20:29:04 -0700

    add postgresql-contrib and create deploy role in postgres

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 sudo apt-add-repository -y ppa:chris-lea/node.js;
 sudo apt-get update;
 sudo apt-get -y upgrade;
-sudo apt-get -y install curl git-core build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libcurl4-openssl-dev nodejs postgresql libpq-dev;
+sudo apt-get -y install curl git-core build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libcurl4-openssl-dev nodejs postgresql postgresql-contrib libpq-dev;
+sudo -u postgres createuser -d -r -s deploy;
 curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash;
 echo '
 export RBENV_ROOT="${HOME}/.rbenv"
