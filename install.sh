f286005a26cf907ca308b96984629bbe4ca46c46
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-03-21 21:00:45 -0700

    use screen session for installing ruby

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt-add-repository -y ppa:chris-lea/node.js;
 sudo apt-get update;
 sudo apt-get -y upgrade;
-sudo apt-get -y install curl git-core build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libcurl4-openssl-dev nodejs postgresql postgresql-contrib libpq-dev;
+sudo apt-get -y install vim screen curl git-core build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libcurl4-openssl-dev nodejs postgresql postgresql-contrib libpq-dev;
 sudo -u postgres createuser -d -r -s deploy;
 curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash;
 echo '
@@ -13,11 +13,4 @@ if [ -d "${RBENV_ROOT}" ]; then
 fi' >> /home/deploy/.bash_profile;
 . /home/deploy/.bash_profile;
 rbenv bootstrap-ubuntu-12-04;
-rbenv install 2.0.0-p481;
-rbenv rehash;
-rbenv global 2.0.0-p481;
-gem update --system;
-gem install bundler;
-rbenv rehash;
-gem install rails;
-rbenv rehash;
+screen -d -m sh install_ruby_rails.sh;
---
+++ install_ruby_rails.sh
@@ -0,0 +1,8 @@
+rbenv install 2.0.0-p481;
+rbenv rehash;
+rbenv global 2.0.0-p481;
+gem update --system;
+gem install bundler;
+rbenv rehash;
+gem install rails;
+rbenv rehash;
