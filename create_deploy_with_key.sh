c1a3d366b4639c5fa65b47e33b4db42587fec5a3
Ryan Loughlin <ryanloughlin25@gmail.com>
2015-03-22 18:11:58 -0700

    learned rackspace will put keys on a server for me.  script now copies keys from where rackspace puts them instead of expecting me to copy it to the server.

--- create_deploy_with_key.sh
+++ create_deploy_with_key.sh
@@ -2,10 +2,8 @@ adduser --disabled-password --gecos "" deploy;
 echo 'deploy:deploy' | chpasswd;
 mkdir /home/deploy/.ssh;
 chmod 700 /home/deploy/.ssh;
-mv /root/id_rsa.pub /home/deploy/.ssh/authorized_keys;
+cp /root/.ssh/authorized_keys /home/deploy/.ssh/authorized_keys;
 chmod 400 /home/deploy/.ssh/authorized_keys;
-mv install.sh /home/deploy;
-mv install_ruby_rails.sh /home/deploy;
 chown deploy:deploy /home/deploy -R
 echo "deploy ALL=(ALL) ALL" >> /etc/sudoers;
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config;
